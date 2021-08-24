0bddbc1cf52da24dbc43db5160f1ee6275ed2eae
Nithi593 <47974050+Nithi593@users.noreply.github.com>
2021-08-24 09:18:52 +0530

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,3 +1,14 @@
 str = input("Enter string : ")
 str1 = str[::-1]
 print(str1)
+
+
+""" 
+    Sample Input:
+    
+    Enter String : Computer Science
+
+    Sample Output:
+
+    ecneicS retupmoC
+"""
