e1b8b23c9342b8cfff4fc6d641f8b923dbddfd8c
Nithi593 <47974050+Nithi593@users.noreply.github.com>
2021-08-31 22:25:50 +0530

    Update Check_if_string _rotaed_2.py

--- Check_if_string _rotaed_2.py
+++ Check_if_string _rotaed_2.py
@@ -1,12 +1,17 @@
 s1 = input("Enter string1 : ")
 s2 = input("Enter string2 : ")
 
-if(len(s1) != len(s2)):
-    print("No")
-elif(sorted(s1) != sorted(s2)):
-    print("No")
-elif("".join(s1[:2]) == "".join(s2[len(s2)-2:])):
-    print("Yes")
+s = s1[2:] + s1[0:2]
+ 
+if s == s2:
+    print("True")
 else:
-    print("No")
+    print("False")
+""" Sample Input:
+    
+    sathesh
+    shhtesa
 
+    sample output:
+    False
+"""
