563f7a210513a9269a71789c34327a99f1beef41
Nithi593 <47974050+Nithi593@users.noreply.github.com>
2021-09-01 22:47:22 +0530

    Add files via upload

---
+++ week2/06_replace_string.py
@@ -0,0 +1,6 @@
+str = input("Enter the string : ")
+key = input("Enter key :")
+replace = input("Enter string : ")
+str = str.replace(key, replace)
+print(str)
+
---
+++ week2/07_add_binary_strings.py
@@ -0,0 +1,7 @@
+s1 = input("Enter string 1 : ")
+s2 = input("Enter string 2 : ")
+
+sum = int(s1,2) + int(s2,2)
+sum = bin(sum)
+
+print(sum)
