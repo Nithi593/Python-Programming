07ffc8c98646d6c68bec0e2a8aa6572004681a15
Nithi593 <47974050+Nithi593@users.noreply.github.com>
2021-09-03 22:03:15 +0530

    Update 06_replace_string.py

--- week2/06_replace_string.py
+++ week2/06_replace_string.py
@@ -4,3 +4,12 @@ replace = input("Enter string : ")
 str = str.replace(key, replace)
 print(str)
 
+"""
+   Sample Input:
+   Enter the string : The cat is barking
+   Enter key : cat
+   Enter string : dog
+   
+   Sample Output:
+   The dog is barking
+"""
